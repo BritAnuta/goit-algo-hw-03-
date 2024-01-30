901087ff2c2a3d7ba7ff9d8dde37e7d445bbd7cb
BritAnuta <brit20121983@gmail.com>
2024-01-30 17:03:34 +0100

    go-algo-hw-03

--- main.py
+++ main.py
@@ -1,73 +1,74 @@
-# # ~ 1 Перше завдання ~
+# ~ 1 Перше завдання ~
 
-# import datetime
+import datetime
 
-# def get_days_from_today(date):
-#     try:
-#         # Парсинг рядка в об'єкт datetime
-#         date_obj = datetime.datetime.strptime(date, '%Y-%m-%d')
-#         # Отримання поточної дати
-#         current_date = datetime.datetime.today().date()
-#         # Рахування різниці в днях
-#         difference = (date_obj.date() - current_date).days
-#         return difference
-#     except Exception:
-#         return "Формат дати введено некоректно"
-# # Отримання дати від користувача
-# user_date = input("Введіть дату у форматі YYYY-MM-DD: ")
-# # Отримання і вивід резутьтату
-# result = get_days_from_today(user_date)
-# print("Кількість днів між заданою датою і поточною датою:", result)
+def get_days_from_today(date):
+    try:
+        # Парсинг рядка в об'єкт datetime
+        date_obj = datetime.datetime.strptime(date, '%Y-%m-%d')
+        # Отримання поточної дати
+        current_date = datetime.datetime.today().date()
+        # Рахування різниці в днях
+        difference = (date_obj.date() - current_date).days
+        return difference
+    except Exception:
+        return "Формат дати введено некоректно"
+# Отримання дати від користувача
+user_date = input("Введіть дату у форматі YYYY-MM-DD: ")
+# Отримання і вивід резутьтату
+result = get_days_from_today(user_date)
+print("Кількість днів між заданою датою і поточною датою:", result)
 
 
-# # ~ Друге завдання ~
+# ~ Друге завдання ~
+# Зробила трошки іншим способом
 
-# import random
+import random
 
-# def get_numbers_ticket(min, max, quantity):
-#  # Встановлюємоємо умови, що повернуть пустий рядок і помилку
-#     if (min < 1) or (max > 1000) or (min > max) or (quantity > (max - min + 1)) or (quantity < 1):
-#         print('Дані введені некоректно')
-#         return []
+def get_numbers_ticket(min, max, quantity):
+ # Встановлюємоємо умови, що повернуть пустий рядок і помилку
+    if (min < 1) or (max > 1000) or (min > max) or (quantity > (max - min + 1)) or (quantity < 1):
+        print('Дані введені некоректно')
+        return []
     
-#     numbers = random.sample(range(min, max+1), quantity) # Вибираємо рандомні числа
-#     return sorted(numbers)                               # Сортуємо їх
-# # Просимо користувача ввести межі, з яких будуть вибрана введена кількість чисел
-# min = int(input("Введіть мінімальне число для рендера від 1-1000: "))
-# max = int(input("Введіть максимальне число для рендера від 2-1000: "))
-# quantity = int(input("Введіть кількість чисел для рендера: "))
+    numbers = random.sample(range(min, max+1), quantity) # Вибираємо рандомні числа
+    return sorted(numbers)                               # Сортуємо їх
+# Просимо користувача ввести межі, з яких будуть вибрана введена кількість чисел
+min = int(input("Введіть мінімальне число для рендера від 1-1000: "))
+max = int(input("Введіть максимальне число для рендера від 2-1000: "))
+quantity = int(input("Введіть кількість чисел для рендера: "))
         
-# # Одержуємо і виводимо результат
-# lottery_numbers = get_numbers_ticket(min, max, quantity)
-# print('Ваші лотерейні числа:',lottery_numbers)
+# Одержуємо і виводимо результат
+lottery_numbers = get_numbers_ticket(min, max, quantity)
+print('Ваші лотерейні числа:',lottery_numbers)
 
 
-# # ~ Третє завдання ~
+# ~ Третє завдання ~
 
-# # Створюємо функцію
-# def normalize_phone(phone_numbers):
-#     intab = "'()[]-/*_ "  # Вказуємо символи, включаючи пробіл, які хочемо видалити з ненормалізованих номерів
-#     normalized_numbers = []  # Створюємо пустий список, щоб у нього вводилися вже нормалізовані номери з циклу
+# Створюємо функцію
+def normalize_phone(phone_numbers):
+    intab = "'()[]-/*_ "  # Вказуємо символи, включаючи пробіл, які хочемо видалити з ненормалізованих номерів
+    normalized_numbers = []  # Створюємо пустий список, щоб у нього вводилися вже нормалізовані номери з циклу
 
-#     # Створюємо цикл, в якому видаляються всі непотрібні символи і перевіряються умови кількості символів у безсимвольному номері
-#     for phone_number in phone_numbers:
-#         trantab = phone_number.maketrans('', '', intab)
-#         num_no_sings = phone_number.translate(trantab)
+    # Створюємо цикл, в якому видаляються всі непотрібні символи і перевіряються умови кількості символів у безсимвольному номері
+    for phone_number in phone_numbers:
+        trantab = phone_number.maketrans('', '', intab)
+        num_no_sings = phone_number.translate(trantab)
 
-#         if len(num_no_sings) == 10:
-#             normalized_numbers.append('+38' + num_no_sings)
-#         elif len(num_no_sings) == 11:
-#             normalized_numbers.append('+3' + num_no_sings)
-#         elif len(num_no_sings) == 12:
-#             normalized_numbers.append('+' + num_no_sings)
-#         else:
-#             normalized_numbers.append(num_no_sings)
+        if len(num_no_sings) == 10:
+            normalized_numbers.append('+38' + num_no_sings)
+        elif len(num_no_sings) == 11:
+            normalized_numbers.append('+3' + num_no_sings)
+        elif len(num_no_sings) == 12:
+            normalized_numbers.append('+' + num_no_sings)
+        else:
+            normalized_numbers.append(num_no_sings)
     
-#     # Повертаємо нормалізовані номера у вигляді списку
-#     return normalized_numbers
+    # Повертаємо нормалізовані номера у вигляді списку
+    return normalized_numbers
 
-# result = normalize_phone(["    +38(050)123-32-34", "     0503451234", "(050)8889900", "38050-111-22-22", "38050 111 22 11   "])
-# print("Нормалізовані номери телефонів для SMS-розсилки:", result)
+result = normalize_phone(["    +38(050)123-32-34", "     0503451234", "(050)8889900", "38050-111-22-22", "38050 111 22 11   "])
+print("Нормалізовані номери телефонів для SMS-розсилки:", result)
 
 
 # ~ Четвертє завдання ~
